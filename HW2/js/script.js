1581ae36d58e5a6eccc7f1e7ab0f92692d62311f
Oleh Khomyshyn <khomyshyn.oleg@gmail.com>
2022-07-08 23:33:52 +0300

    HW2 added

---
+++ HW2/js/script.js
@@ -0,0 +1,26 @@
+let userName = prompt('Enter your name');
+while (!userName) {
+  prompt('Please enter your name again', userName);
+}
+
+let userAge = +prompt('Enter your age');
+while (!userAge && isNaN(userAge)) {
+  +prompt('Please enter your age again', userAge);
+}
+
+if (userAge < 18) {
+  alert('You are not allowed to visit this website');
+}
+
+else if (userAge > 22) {
+  alert('Welcome, ' + userName);
+}
+
+else if (userAge >= 18 && userAge <= 22) {
+  let userAnswer = confirm('Are you sure you want to continue?');
+  if (userAnswer == true) {
+    alert('Welcome, ' + userName);
+  } else {
+    alert('You are not allowed to visit this website');
+  }
+}
