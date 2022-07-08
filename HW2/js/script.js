2eda91c44aec717b54313a3221a53d04cc9756a1
Oleh Khomyshyn <khomyshyn.oleg@gmail.com>
2022-07-08 23:43:56 +0300

    update HW2

--- HW2/js/script.js
+++ HW2/js/script.js
@@ -1,22 +1,25 @@
-let userName = prompt('Enter your name');
+let userName = "";
+userName = prompt('Enter your name');
 while (!userName) {
-  prompt('Please enter your name again', userName);
+  alert('Name is invalid. Try again');
+  userName = prompt('Please enter your name again', userName);
 }
 
-let userAge = +prompt('Enter your age');
-while (!userAge && isNaN(userAge)) {
-  +prompt('Please enter your age again', userAge);
+let userAge = null;
+userAge = +prompt('Enter your age');
+while (!userAge || isNaN(userAge)) {
+  alert('Age is invalid. Try again');
+  userAge = +prompt('Please enter your age again', userAge);
 }
 
 if (userAge < 18) {
   alert('You are not allowed to visit this website');
-}
-
+} 
 else if (userAge > 22) {
   alert('Welcome, ' + userName);
 }
 
-else if (userAge >= 18 && userAge <= 22) {
+else if (userAge >= 18 || userAge == 22) {
   let userAnswer = confirm('Are you sure you want to continue?');
   if (userAnswer == true) {
     alert('Welcome, ' + userName);
