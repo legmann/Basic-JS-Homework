91d1afd5d2c3c1f797492abb8fac2a2c4df3d1c6
Oleh Khomyshyn <khomyshyn.oleg@gmail.com>
2022-07-14 18:08:35 +0300

    HW 4 add

---
+++ HW4/js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let num1 = prompt('Enter num1');
+let num2 = prompt('Enter num2');
+
+while (!num1 || isNaN(num1) || !num2 || isNaN(num2)) {
+  num1 = prompt('Try again, Please enter the num1', num1);
+  num2 = prompt('Try again, Please enter the num2', num2);
+}
+
+let mathOp = prompt('Enter operation + - * /');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+function calc() {
+  let result;
+
+  switch (mathOp) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "+":
+      result = num1 + num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      alert('Error, use only +, -, *, / operation');
+  }
+
+  return result;
+}
+
+console.log(calc());
