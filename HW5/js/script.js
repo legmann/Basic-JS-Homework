c012bc2f840be0e519e433e9edeaaff7fa2bc937
Oleh Khomyshyn <khomyshyn.oleg@gmail.com>
2022-07-17 23:00:08 +0300

    HW5 bugFix

--- HW5/js/script.js
+++ HW5/js/script.js
@@ -1,36 +1,41 @@
 function createNewUser() {
-
-  let firstName = prompt('Please enter your firstName');
-  let lastName = prompt('Please enter your lastName');
-
+  let firstName = prompt("Please enter your firstName");
+  let lastName = prompt("Please enter your lastName");
 
   const newUser = Object.create(
     {
       getLogin() {
-        let newLogin = this.firstName.charAt(0).toLowerCase() + this.lastName.toLowerCase();
-        return newLogin;
-      }
-
+        return (this.firstName.charAt(0) + this.lastName).toLowerCase();
+      },
+      setFirstName(name) {
+        Object.defineProperty(this, "firstName", { writable: true });
+        this.firstName = name;
+        Object.defineProperty(this, "firstName", { writable: false });
+      },
+      setLastName(name) {
+        Object.defineProperty(this, "lastName", { writable: true });
+        this.lastName = name;
+        Object.defineProperty(this, "lastName", { writable: false });
+      },
     },
     {
       firstName: {
         value: firstName,
-        enumerable: false,
         writable: false,
-        configurable: false
+        configurable: true,
       },
 
       lastName: {
         value: lastName,
-        enumerable: false,
         writable: false,
-        configurable: false
-      }
+        configurable: true,
+      },
     }
-  )
+  );
 
   return newUser;
 }
 
-let newUser = new createNewUser();
-console.log(newUser.getLogin());
+const user = createNewUser();
+
+console.log(user.getLogin());
