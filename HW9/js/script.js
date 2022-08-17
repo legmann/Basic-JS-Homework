2a18ccb0b776fa3b1c029cc6436ccff17595340e
legmann <khomyshyn.oleg@gmail.com>
2022-08-17 19:37:01 +0300

    added HW9 fix

--- HW9/js/script.js
+++ HW9/js/script.js
@@ -2,23 +2,14 @@
 
 function out(array, parent = document.body) {
 
-  let item = array.map((item) => `<li>${item}</li>`);
-  let li = '';
-
-  for (let i = 0; i < item.length; i++) {
-    li += item[i];
-  }
-
-  let ul = `<ul>${li}</ul>`;
+  let items = array.map((item) => `<li>${item}</li>`).join('');
 
   if (parent !== document.body) {
-    let createParent = document.createElement(parent);
-    document.body.append(createParent);
-    createParent.innerHTML = ul;
-  } else {
-    document.body.innerHTML = ul;
+    parent = document.createElement(parent);
+    document.body.append(parent);
   }
 
+  parent.insertAdjacentHTML('afterbegin', `<ul>${items}</ul>`);
 }
 
 out(["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"], 'div');
